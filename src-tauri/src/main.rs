90a3efd5ae0239fcaf43f90bdc8859d9e3f6bac6
Jade Turner <spacey-sooty@proton.me>
2024-08-30 13:22:33 +0800

    use extension type for RobotMode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fmt;
+
 use ds::{DriverStation, Mode};
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{self, Visitor},
+    Deserialize, Serialize,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum AllianceColour {
@@ -17,12 +22,49 @@ pub enum RobotState {
     Estopped,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub enum RobotMode {
-    Teleop,
-    Auto,
-    Test,
+#[derive(Debug)]
+pub struct RobotMode(Mode);
+
+impl Serialize for RobotMode {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        format!("{:?}", self).serialize(serializer)
+    }
 }
+
+impl<'de> Deserialize<'de> for RobotMode {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct ModeVisitor;
+
+        impl<'d> Visitor<'d> for ModeVisitor {
+            type Value = RobotMode;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("`secs` or `nanos`")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<RobotMode, E>
+            where
+                E: de::Error,
+            {
+                match value {
+                    "Teleop" | "Teleoperated" => Ok(RobotMode(Mode::Teleoperated)),
+                    "Auto" | "Autonomous" => Ok(RobotMode(Mode::Autonomous)),
+                    "Test" => Ok(RobotMode(Mode::Test)),
+                    _ => Err(de::Error::unknown_field(value, MODE_FIELDS)),
+                }
+            }
+        }
+
+        deserializer.deserialize_identifier(ModeVisitor)
+    }
+}
+const MODE_FIELDS: &[&str] = &["Teleop", "Auto", "Test"];
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Packet {
@@ -57,11 +99,7 @@ fn send_packet(packet: Packet) {
         }
     }
 
-    match packet.mode {
-        RobotMode::Auto => connection.set_mode(Mode::Autonomous),
-        RobotMode::Teleop => connection.set_mode(Mode::Teleoperated),
-        RobotMode::Test => connection.set_mode(Mode::Test),
-    }
+    connection.set_mode(packet.mode.0);
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -71,7 +109,10 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let connection = DriverStation::new_team(9999, ds::Alliance::new_red(0));
+
     tauri::Builder::default()
+        .manage(connection)
         .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![send_packet])
         .run(tauri::generate_context!())
